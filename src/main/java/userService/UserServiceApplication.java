5d4a4880daaf93b53ef16bab063964b7fb7412df
Admin <erarvinder2012@gmail.com>
2025-11-16 20:45:11 +0200

    java

--- src/main/java/userService/UserServiceApplication.java
+++ src/main/java/userService/UserServiceApplication.java
@@ -3,11 +3,10 @@ package userService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
 @SpringBootApplication
-		(exclude = SecurityAutoConfiguration.class)
+//		(exclude = SecurityAutoConfiguration.class)
 //@SpringBootApplication(exclude = {MailSenderAutoConfiguration.class})
 public class UserServiceApplication {
 
