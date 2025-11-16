24d4b14142fc0fe663052e0e78c0a982dc8ca2ec
Admin <erarvinder2012@gmail.com>
2025-11-16 20:27:08 +0200

    java mail

--- src/main/java/userService/UserServiceApplication.java
+++ src/main/java/userService/UserServiceApplication.java
@@ -3,14 +3,11 @@ package userService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 
 
-//@SpringBootApplication
+@SpringBootApplication
 //		(exclude = SecurityAutoConfiguration.class)
-@SpringBootApplication(exclude = {
-        MailSenderAutoConfiguration.class
-})
+
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
